ff58925483c0e7f75d9f3b5d564801cbb4c7a3e5
Dario Mintzer <dmintzer75@hotmail.com>
2022-03-05 10:55:34 +0100

    Initial Commit

---
+++ DarioMintzerCard/DarioMintzerCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarioMintzerCardApp.swift
+//  DarioMintzerCard
+//
+//  Created by Dario Mintzer on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DarioMintzerCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
